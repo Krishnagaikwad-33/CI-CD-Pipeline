b150faf8b9573a61abb4712869e8d93c9a48e692
Krishnagaikwad-33 <24kgaikwad33@gmail.com>
2025-05-30 07:04:57 +0000

    backend disabled

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,8 +1,11 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "avd-training-rg"
-    storage_account_name = "tfstatestoragebackend33"
-    container_name       = "tfstate"
-    key                  = "network.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "avd-training-rg"
+#     storage_account_name = "tfstatestoragebackend33"
+#     container_name       = "tfstate"
+#     key                  = "network.terraform.tfstate"
+#   }
+# }
+
+
+#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
