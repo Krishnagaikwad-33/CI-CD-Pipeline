76377648d67c891a74a44df6615da80943dd756e
Krishnagaikwad-33 <24kgaikwad33@gmail.com>
2025-06-06 07:19:41 +0000

    correct the backend.tf file

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-33"
+    bucket         = "terraform-backend33"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
