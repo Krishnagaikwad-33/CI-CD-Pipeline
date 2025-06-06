8c34fe906689e9138c0d9d61cd39435b98102bfa
Krishnagaikwad-33 <24kgaikwad33@gmail.com>
2025-06-06 07:39:29 +0000

    correct the backend file and updatw the bucket policy

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend33"
+    bucket         = "terraform-backend34"
     key            = "dev/terraform.tfstate"
     dynamodb_table = "terraform-locks"
     encrypt        = true
