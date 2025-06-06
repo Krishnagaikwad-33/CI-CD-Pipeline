404c6530c134a4b135fc6686fb151401efca3bbc
Krishnagaikwad-33 <24kgaikwad33@gmail.com>
2025-06-06 07:31:21 +0000

    adjusted according to region and removed files

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend33"
     key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
